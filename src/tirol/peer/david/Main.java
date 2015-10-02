e714869d8e5d0a6cb4ef8b1e107057f3674449bc
david.peer <david@peer.tirol>
2015-10-02 23:15:41 +0200

    Added some java messages

--- src/tirol/peer/david/Main.java
+++ src/tirol/peer/david/Main.java
@@ -3,6 +3,8 @@ package tirol.peer.david;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("-------------------------");
         System.out.println("Hello java, docker, ci, github etc.");
+        System.out.println("-------------------------");
     }
 }
