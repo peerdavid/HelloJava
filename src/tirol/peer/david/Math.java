add2ae0b991ecdb282f5b83c9c38838c61caaed1
david.peer <david@peer.tirol>
2015-10-03 11:43:18 +0200

    Deleted old project

---
+++ src/tirol/peer/david/Math.java
@@ -0,0 +1,7 @@
+package tirol.peer.david;
+
+/**
+ * Created by david on 03.10.15.
+ */
+public class Math {
+}
---
+++ tests/tirol/peer/david/MathTest.java
@@ -0,0 +1,10 @@
+package tirol.peer.david;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david on 03.10.15.
+ */
+public class MathTest {
+
+}
